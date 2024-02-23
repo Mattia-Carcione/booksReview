3d2eddfb0273c933a405c0596145b30ddedd0a2c
Mattia <mattia@MacBook-Pro-di-Mattia.local>
2024-02-23 18:09:47 +0100

    add header, footer and style

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// <-- Import dependecies
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+// END dependencies --> 
 
+// <-- Setup backend
 const app = express();
 const port = 3000;
 
@@ -16,15 +19,19 @@ db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+// END setup-->
 
+// <-- Routes
 app.get("/", (req, res) => {
     try {
         res.render("index.ejs");
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 });
+// END routes -->
 
+// Setup server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
